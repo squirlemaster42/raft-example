8d144629bb439d67b20aadb319d8950ba2d412ac
squirlemaster42 <jakobmisbach8@gmail.com>
2025-07-01 23:27:23 -0400

    Started building raft

---
+++ raft.go
@@ -0,0 +1,187 @@
+package raftexample
+
+//TODO Left off on becoming a leader
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"sync"
+	"time"
+)
+
+const DebugCM = 1
+
+type LogEntry struct {
+    Command any
+    Term int
+}
+
+type CMState int
+
+const (
+    Follower CMState = iota
+    Candidate
+    Leader
+    Dead
+)
+
+func (s CMState) String() string {
+    switch s {
+    case Follower:
+        return "Follower"
+    case Candidate:
+        return "Candidate"
+    case Leader:
+        return "Leader"
+    case Dead:
+        return "Dead"
+    default:
+        panic("unknown CM state")
+    }
+}
+
+type ConsensusModule struct {
+    mu sync.Mutex
+    id int
+    peerIds []int
+    server *Server
+    currentTerm int
+    votedFor int
+    log []LogEntry
+
+    state CMState
+    electionResetEvent time.Time
+}
+
+func NewConsensusModule(id int, peerIds []int, server *Server, ready <-chan any) *ConsensusModule {
+    cm := new(ConsensusModule)
+    cm.id = id
+    cm.peerIds = peerIds
+    cm.server = server
+    cm.state = cm.state
+    cm.votedFor = -1
+
+    go func() {
+        <-ready
+        cm.mu.Lock()
+        cm.electionResetEvent = time.Now()
+        cm.mu.Unlock()
+        cm.runElectionTimer()
+    }()
+
+    return cm
+}
+
+func (cm *ConsensusModule) dlog(format string, args ...any) {
+    if DebugCM > 0 {
+        format = fmt.Sprintf("[%d] ", cm.id) + format
+        log.Printf(format, args...)
+    }
+}
+
+type RequestVoteArgs struct {
+    Term int
+    CandidateId int
+    LastLogIndex int
+    LastLogTerm int
+}
+
+type RequestVoteReply struct {
+    Term int
+    VotesGranted bool
+}
+
+func (cm *ConsensusModule) electionTimeout() time.Duration {
+    //Create more colisions and reelections if we are testing
+    if len(os.Getenv("RAFT_FORCE_MORE_REELECTION")) > 0 && rand.Intn(3) == 0 {
+        return time.Duration(150) * time.Millisecond
+    } else {
+        return time.Duration(150 + rand.Intn(150)) * time.Millisecond
+    }
+}
+
+func (cm *ConsensusModule) runElectionTimer() {
+    timeoutDuration := cm.electionTimeout()
+    cm.mu.Lock()
+    termStarted := cm.currentTerm
+    cm.mu.Unlock()
+    cm.dlog("election timer started (%v), term=%d", timeoutDuration, termStarted)
+
+    ticker := time.NewTicker(10 * time.Millisecond)
+    defer ticker.Stop()
+    for {
+        <-ticker.C
+
+        cm.mu.Lock()
+        if cm.state != Candidate && cm.state != Follower {
+            cm.dlog("in election timer state=%s, bailing out", cm.state)
+            cm.mu.Unlock()
+            return
+        }
+
+        if termStarted != cm.currentTerm {
+            cm.dlog("in election timer term changed from %d to %d, bailing out", termStarted, cm.currentTerm)
+            cm.mu.Unlock()
+            return
+        }
+
+        if elapsed := time.Since(cm.electionResetEvent); elapsed >= timeoutDuration {
+            cm.startElection()
+            cm.mu.Unlock()
+            return
+        }
+        cm.mu.Unlock()
+    }
+}
+
+func (cm *ConsensusModule) startElection() {
+    cm.state = Candidate
+    cm.currentTerm += 1
+    savedCurrentTerm := cm.currentTerm
+    cm.electionResetEvent = time.Now()
+    cm.votedFor = cm.id
+    cm.dlog("becomes Candidate (currentTerm=%d); log=%v", savedCurrentTerm, cm.log)
+
+    votesReceived := 1
+
+    for _, peerId := range cm.peerIds {
+        go func(peerId int) {
+            args := RequestVoteArgs {
+                Term: savedCurrentTerm,
+                CandidateId: cm.id,
+            }
+            var reply RequestVoteReply
+
+            cm.dlog("sending RequestVote to %d: %+v", peerId, args)
+            if err := cm.server.Call(peerId, "ConsensusModule.RequestVote", args, &reply); err == nil {
+                cm.mu.Lock()
+                defer cm.mu.Unlock()
+                cm.dlog("reveived RequestVoteReply %+v", reply)
+
+                if cm.state != Candidate {
+                    cm.dlog("while waiting for reply, state=%v", cm.state)
+                    return
+                }
+
+                if reply.Term > savedCurrentTerm {
+                    cm.dlog("term out of date in RequestVoteReply")
+                    cm.becomeFollower(reply.Term)
+                    return
+                } else if reply.Term == savedCurrentTerm {
+                    if reply.VoteGranted {
+                        votesReceived++
+                        if votesReceived * 2 > len(cm.peerIds) + 1 {
+                            cm.dlog("wins election with %d votes", votesReceived)
+                            cm.startLeader()
+                            return
+                        }
+                    }
+                }
+            }
+        }(peerId)
+    }
+
+    go cm.runElectionTimer()
+}
