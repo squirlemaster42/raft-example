2a92c5b03ea92cec99c9d041cef560807ef97efa
squirlemaster42 <jakobmisbach8@gmail.com>
2025-07-03 17:55:05 -0400

    started writing testing stuff

---
+++ part1/testharness.go
@@ -0,0 +1,75 @@
+package raftexample
+
+import (
+	"log"
+	"testing"
+)
+
+func init() {
+    log.SetFlags(log.Ltime | log.Lmicroseconds)
+}
+
+type Harness struct {
+    cluster []*Server
+    connected []bool
+    n int
+    t *testing.T
+}
+
+func NewHarness(t *testing.T, n int) *Harness {
+    ns := make([]*Server, n)
+    connected := make([]bool, n)
+    ready := make(chan any)
+
+    for i := 0; i < n; i++ {
+        peerIds := make([]int, 0)
+        for p := 0; p < n; p++ {
+            if p != i {
+                peerIds = append(peerIds, p)
+            }
+        }
+
+        ns[i] = NewServer(i, peerIds, ready)
+        ns[i].Serve()
+    }
+
+    for i := 0; i < n; i++ {
+        for j := 0; j < n; j++ {
+            if i != j {
+                ns[i].ConnectToPeer(j, ns[i].GetListenAddr())
+            }
+        }
+        connected[i] = true
+    }
+    close(ready)
+
+    return &Harness{
+        cluster: ns,
+        connected: connected,
+        n: n,
+        t: t,
+    }
+}
+
+func (h *Harness) Shutdown() {
+    for i := 0; i < h.n; i++ {
+        h.cluster[i].DisconnectAll()
+        h.connected[i] = false
+    }
+    for i := 0; i < h.n; i++ {
+        h.cluster[i].Shutdown()
+    }
+}
+
+func (h *Harness) DisconnectPeer(id int) {
+    tlog("Disconnect %d", id)
+    h.cluster[id].DisconnectAll()
+    for j := 0; j < h.n; j++ {
+        if j != id {
+            h.cluster[j].DisconnectPeer(id)
+        }
+    }
+    h.connected[id] = false
+}
+
+//TODO Next is ReconnectPeer
